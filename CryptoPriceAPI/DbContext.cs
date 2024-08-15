d4c161d29fe4dd43df44936ac44383d6348658f0
MELIH YILDIZ (TECHNOLOGY) <DTMEYILDIZ@digiturk.com.tr>
2024-08-15 16:44:11 +0300

    deneme

---
+++ CryptoPriceAPI/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class CryptoDbContext : DbContext
+{
+    public CryptoDbContext(DbContextOptions<CryptoDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<CryptoCurrency> CryptoCurrencies { get; set; }
+}
